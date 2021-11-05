d5de7d306808dd0da9b87a81eef85e53f9010562
Maximilian Merz <info+git@merzmax.de>
2021-11-05 12:54:11 +0100

    add a basic http server with an /hello endpoint

--- implementation/server/main.go
+++ implementation/server/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
-	println("Hello World!")
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(writer http.ResponseWriter, req *http.Request) {
+	log.Println("/hello endpoint requested")
+	writer.Write([]byte("Hello World!"))
+	return
 }
