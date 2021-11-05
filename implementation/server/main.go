d5a63a9d0db3663da59b6f767da0bd75f1d0a64f
Maximilian Merz <info+git@merzmax.de>
2021-11-05 13:08:13 +0100

    basic ws server

--- implementation/server/main.go
+++ implementation/server/main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
+	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 )
 
+var upgrader = websocket.Upgrader{} // use default options
+
 func main() {
 	http.HandleFunc("/hello", hello)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/ws", upgradeToWs)
+	err := http.ListenAndServe(":8080", nil)
+	checkError(err)
+}
+
+func upgradeToWs (writer http.ResponseWriter, req *http.Request) {
+	wsConnection, err := upgrader.Upgrade(writer, req, nil)
+	checkError(err)
+	defer wsConnection.Close()
+
+	for {
+		messageType, message, err := wsConnection.ReadMessage()
+		checkError(err)
+
+		log.Printf("message: %s", message)
+		log.Printf("messageType: %d", messageType)
+
+		err = wsConnection.WriteMessage(messageType, []byte("Hello you"))
+		checkError(err)
+	}
+
+
 }
 
 func hello(writer http.ResponseWriter, req *http.Request) {
 	log.Println("/hello endpoint requested")
 	writer.Write([]byte("Hello World!"))
 	return
+}
+
+func checkError(err error){
+	if err != nil {
+		log.Fatal(err)
+	}
 }
