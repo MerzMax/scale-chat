814cf6ec446f59697d3a4fe94cf6d28e0adbd2e6
Martin Bock <hello@martin-bock.com>
2021-12-08 16:26:20 +0100

    Reformat

--- src/client/MessageMonitor.go
+++ src/client/MessageMonitor.go
@@ -18,18 +18,16 @@ func (t Type) String() string {
 
 type MessageEventEntry struct {
 	MessageId uint64
-	ClientId string
-	Type Type
+	ClientId  string
+	Type      Type
 	TimeStamp time.Time
 }
 
 func (m MessageEventEntry) ConvertToStringArray() []string {
-	return []string{strconv.FormatUint(m.MessageId, 10),
-					m.ClientId,
-					m.Type.String(),
-					strconv.FormatUint(uint64(m.TimeStamp.UnixMicro()), 10)}
+	return []string{
+		strconv.FormatUint(m.MessageId, 10),
+		m.ClientId,
+		m.Type.String(),
+		strconv.FormatUint(uint64(m.TimeStamp.UnixMicro()), 10),
+	}
 }
-
-
-
-
